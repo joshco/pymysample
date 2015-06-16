fb4a4bd0b035b31e1a4c0a16c5bb88a4fc1f7496
Josh Cohen <joshco@gmail.com>
2015-06-15 21:18:24 -0400

    new model

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^sentiment$',views.sentiment),
     url(r'^$', views.index, name='index'),
     url(r'^(?P<folk_id>[0-9]+)/$', views.detail, name='detail')
+  
 ]
